fce0bf9faea0bb34388942b8bd6a6ebce3ad9aaa
Lucy <leticiadantas2001.14@gmail.com>
2025-12-09 14:12:22 -0300

    Create operações.py

---
+++ 01-exercicios-basicos/operacoes.py
@@ -0,0 +1,24 @@
+# Exercícios de operações básicas
+
+# 1. Some dois números
+a = 10
+b = 4
+print("Soma:", a + b)
+
+# 2. Subtração
+print("Subtração:", a - b)
+
+# 3. Multiplicação
+print("Multiplicação:", a * b)
+
+# 4. Divisão
+print("Divisão:", a / b)
+
+# 5. Divisão inteira
+print("Divisão inteira:", a // b)
+
+# 6. Resto
+print("Resto:", a % b)
+
+# 7. Potência
+print("Potência:", a ** b)
