9dc89fba32c0e7b225625e67ebb0addc0b23280e
Lucy <leticiadantas2001.14@gmail.com>
2025-12-10 00:17:51 -0300

    Create desafio_listas.py

---
+++ 03-desafios/desafio_listas.py
@@ -0,0 +1,14 @@
+# Desafio: Trabalhando com listas
+# Dada uma lista de números, exiba:
+# - o maior número
+# - o menor número
+# - a soma de todos os valores
+# - a média dos valores
+
+numeros = [4, 7, 1, 9, 3, 10, 2]
+
+print("Lista:", numeros)
+print("Maior número:", max(numeros))
+print("Menor número:", min(numeros))
+print("Soma:", sum(numeros))
+print("Média:", sum(numeros) / len(numeros))
